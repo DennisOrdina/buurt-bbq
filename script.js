d21a8ee67f134e11e346edd784cb778ce6a8f699
Dennis Rosema <dennis.rosema@ordina.nl>
2026-09-04 16:16:12 +0200

    Add current kids songs to music quiz

--- script.js
+++ script.js
@@ -4,35 +4,35 @@ const questionData = [
   ["1980s", "Uit welke Amerikaanse stad komt bokser Rocky Balboa?", "Film", ["Chicago", "Philadelphia", "Boston", "Detroit"], 1, "2HHtWyy5CgaQbC7XSoOb0e"],
   ["1970s", "Waarvoor staan de letters ELO?", "Muziekgeschiedenis", ["Electric Light Orchestra", "European Live Orchestra", "Electric Love Organisation", "English Light Orchestra"], 0, "2RlgNHKcydI9sayD2Df2xp"],
   ["1970s", "In welke stad ligt de straat die dit nummer zijn titel gaf?", "Muziekgeschiedenis", ["Londen", "Edinburgh", "Dublin", "Liverpool"], 0, "2x7JPx4lQ6xm4ba5ughsvM"],
-  ["1980s", "Welke animatietechniek maakt de videoclip bij dit nummer zo herkenbaar?", "Videoclip", ["Rotoscoping", "Stop-motion", "Kleianimatie", "Pixilation"], 0, "2WfaOiMkCvy7F5fcp2zZ8L"],
-  ["1990s", "Welke acteur speelde Ross Geller in de sitcom bij dit nummer?", "TV-serie", ["Matt LeBlanc", "David Schwimmer", "Matthew Perry", "Paul Rudd"], 1, "7MZOtq8HEcSVxDHc4kDGWF"],
+  ["Kids", "Welke groep zingt dit liedje?", "Kindervraag", ["Turfy Gang", "Bankzitters", "K3", "De Bellinga's"], 1, "7febA9EPgU97DuLg9HAocX"],
+  ["Kids", "Hoe heet dit liedje van Turfy Gang en Russo?", "Kindervraag", ["Padellen", "Bacotrein", "Ushuaia", "After van de After"], 2, "72LwJNYsbIBt7QbVf4X67P"],
   ["1990s", "Welke Spice Girl droeg de bijnaam 'Sporty Spice'?", "Popcultuur", ["Mel C", "Emma Bunton", "Mel B", "Geri Halliwell"], 0, "1Je1IMUlBXcx1Fz0WE7oPT"],
-  ["1990s", "Welke regisseur maakte de film bij deze iconische ballad?", "Film", ["Steven Spielberg", "James Cameron", "Ridley Scott", "Ron Howard"], 1, "33LC84JgLvK2KuW43MfaNq"],
+  ["Kids", "Wie zingt dit liedje?", "Kindervraag", ["Lucilla Bellinga", "Roxy Dekker", "Luna Sabella", "Sophie"], 0, "6xziwyOQXFh4sdTunaP5sm"],
   ["2010s", "In welk jaar werd dit nummer uitgebracht?", "Muziekgeschiedenis", ["2017", "2019", "2021", "2023"], 1, "3RauEVgRgj1IuWdJ9fDs70"],
-  ["2000s", "Welke acteur speelt de hoofdrol in de dansfilm die bij dit nummer hoort?", "Film", ["Zac Efron", "Channing Tatum", "Ryan Gosling", "Chris Evans"], 1, "4JpKVNYnVcJ8tuMKjAj50A"],
+  ["1980s", "Welke animatietechniek maakt de videoclip bij dit nummer zo herkenbaar?", "Videoclip", ["Rotoscoping", "Stop-motion", "Kleianimatie", "Pixilation"], 0, "2WfaOiMkCvy7F5fcp2zZ8L"],
   ["1980s", "Welke band zingt dit nummer?", "Artiest", ["Guns N' Roses", "Bon Jovi", "Aerosmith", "AC/DC"], 0, "7o2CTH4ctstm8TNelqjb51"],
   ["2000s", "Welk land hoort bij deze wereldhit 'Dragostea Din Tei'?", "Land", ["Roemenië", "Moldavië", "Bulgarije", "Hongarije"], 1, "7CnnGGBLuuu9cawoiHPLl4"],
   ["2000s", "Welke titel droeg Beyoncé's eerste soloalbum, waarop dit nummer stond?", "Album", ["B'Day", "Dangerously in Love", "4", "I Am... Sasha Fierce"], 1, "5IVuqXILoxVWvWEPm82Jxr"],
   ["2000s", "Welke voornaam heeft de zanger van The Killers?", "Artiest", ["Dave", "Brandon", "Brendan", "Bryan"], 1, "003vvx7Niy0yvhvHt4a68B"],
-  ["2010s", "Welke acteur spreekt de stem in van Gru uit de film bij dit nummer?", "Film", ["Steve Carell", "Ben Stiller", "Jim Carrey", "Owen Wilson"], 0, "60nZcImufyMA1MKQY3dcCH"],
+  ["Kids", "Wie zingt dit liedje?", "Kindervraag", ["Roxy Dekker", "Luna Sabella", "Flemming", "Maan"], 0, "3A08L1HFsnHKyFQep0AWyY"],
   ["2010s", "Uit welk land komt het duo Daft Punk?", "Muziekgeschiedenis", ["België", "Frankrijk", "Canada", "Verenigd Koninkrijk"], 1, "69kOkLUCkxIZYexIgSG8rq"],
-  ["2000s", "Welke groep zingt dit nummer?", "Artiest", ["The Pussycat Dolls", "Black Eyed Peas", "OutKast", "LMFAO"], 1, "4vp2J1l5RD4gMZwGFLfRAu"],
-  ["2010s", "In welk land werd het WK voetbal van 2010 gehouden?", "Sport", ["Brazilië", "Zuid-Afrika", "Duitsland", "Japan"], 1, "6bNB5gxFX6Q87DbQWb8OWZ"],
+  ["Kids", "Welke twee artiesten zingen dit liedje?", "Kindervraag", ["K3 en Donnie", "Russo en Roxy", "K3 en Claude", "Donnie en Flemming"], 0, "2mUcEKURSlbE255TJKDuTl"],
+  ["Kids", "Wie zingen dit liedje samen?", "Kindervraag", ["De Bankzitters", "Kinderen voor Kinderen", "De finalisten van het Junior Songfestival", "De Bellinga's"], 2, "7nbjBe061VcpdSMlfKqmca"],
   ["2010s", "Welke acteur speelt naast Lady Gaga in de film bij deze duet-hit?", "Film", ["Ryan Gosling", "Bradley Cooper", "Hugh Jackman", "Matt Damon"], 1, "2VxeLyX666F8uXCJ0dZF8B"],
   ["2020s", "In welk jaar verscheen dit Kate Bush-nummer oorspronkelijk?", "Muziekgeschiedenis", ["1979", "1985", "1991", "1997"], 1, "29d0nY7TzCoi22XBqDQkiP"],
   ["2020s", "Wat is de voornaam van The Weeknd?", "Artiest", ["Abel", "Adam", "Amir", "Alex"], 0, "0VjIjW4GlUZAMYd2vXMi3b"],
   ["2020s", "Op welk ouder nummer wordt deze hit vaak gezien als een antwoord?", "Muziekverband", ["When I Was Your Man", "Grenade", "Locked Out of Heaven", "Just the Way You Are"], 0, "4DHcnVTT87F0zZhRPYmZ3B"],
   ["1990s", "Welke band zingt dit nummer?", "Artiest", ["The Corrs", "The Cranberries", "U2", "The Cardigans"], 1, "7EZC6E7UjZe63f1jRmkWxt"],
   ["2020s", "In welk land vond het Eurovisie Songfestival van 2024 plaats?", "Eurovisie", ["Zweden", "Nederland", "Zwitserland", "Oostenrijk"], 0, "0uHrMbMv3c78398pIANDqR"],
-  ["2020s", "Op welk album van Sabrina Carpenter verscheen dit nummer?", "Album", ["Short n' Sweet", "emails i can't send", "Singular: Act II", "Evolution"], 0, "2qSkIjg1o9h3YT9RAgYN75"],
+  ["Kids", "Welk kinderkoor zingt dit liedje?", "Kindervraag", ["Kinderen voor Kinderen", "K3", "Junior Songfestival", "NXT"], 0, "0Ul3G0uGWfI2qPYjVVv4ix"],
   ["2010s", "Onder welke artiestennaam werd Tim Bergling wereldberoemd?", "Artiest", ["Kygo", "Avicii", "Calvin Harris", "Martin Garrix"], 1, "0nrRP2bk19rLc0orkWPQk2"],
   ["2020s", "In welke stad werd Goldband opgericht?", "Nederlandse muziek", ["Den Haag", "Utrecht", "Groningen", "Eindhoven"], 0, "2LcmbuYX7tyR4DWy3b273L"],
-  ["1970s", "Welke band zingt dit nummer?", "Artiest", ["Queen", "The Rolling Stones", "The Beatles", "Pink Floyd"], 0, "4u7EnebtmKWzUH433cf5Qv"],
+  ["Kids", "Hoe heet dit nieuwe zomerliedje van K3?", "Kindervraag", ["Waterval", "De zomer van Oya Lélé", "Mango Mango", "Zomer van liefde"], 1, "781CYhpS8TdpVN9Uosyu3e"],
   ["1970s", "In welk decennium werd dit disco-nummer uitgebracht?", "Muziekgeschiedenis", ["1960s", "1970s", "1980s", "1990s"], 1, "2grjqo0Frpf2okIBiifQKs"]
 ].map(([decade, question, category, answers, correct, spotifyId]) => ({ decade, question, category, answers, correct, spotifyId }));
 
 // Vaste, gemengde quizvolgorde. Zo blijft de antwoordenlijst steeds gelijk aan de vragenronde.
-const questionOrder = [16, 4, 25, 8, 19, 2, 28, 12, 6, 22, 14, 1, 27, 9, 21, 11, 3, 24, 18, 7, 29, 15, 10, 23, 5, 26, 13, 20, 0, 17];
+const questionOrder = [16, 25, 4, 19, 2, 8, 12, 22, 14, 28, 1, 27, 9, 6, 21, 11, 3, 18, 24, 7, 29, 15, 10, 23, 26, 5, 13, 20, 0, 17];
 const questions = questionOrder.map((index) => questionData[index]);
 
 let current = 0;
